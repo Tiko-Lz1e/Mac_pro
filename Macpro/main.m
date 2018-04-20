b9e4a81b3219b78a195284038710d78d29023426
Twx1213 <30528285+Twx1213@users.noreply.github.com>
2018-04-20 09:56:27 +0800

    Initial Commit

---
+++ Macpro/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Macpro
+//
+//  Created by 汤文骁 on 2018/4/20.
+//  Copyright © 2018年 twx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
